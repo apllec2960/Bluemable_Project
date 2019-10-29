12af9fb92431cfc9bc822775c919e3a3515156b1
이정원 <apllec2960@naver.com>
2019-10-29 18:18:34 +0900

    게임 시작전 버튼, 주사위, 사용자 말 추가 및 숨기기 적용. css 코드 추가 (반투명화면) 주사위값 변수명 변경. 주사위 이미지 추가.

--- bluemable/WebContent/script/game.js
+++ bluemable/WebContent/script/game.js
@@ -1,3 +1,22 @@
+let face1=new Image()
+	face1.src="http://www.blueb.co.kr/SRC/javascript/image1/d1.gif"
+	
+let face2=new Image()
+	face2.src="http://www.blueb.co.kr/SRC/javascript/image1/d2.gif"
+	
+let face3=new Image()
+	face3.src="http://www.blueb.co.kr/SRC/javascript/image1/d3.gif"
+	
+let face4=new Image()
+	face4.src="http://www.blueb.co.kr/SRC/javascript/image1/d4.gif"
+	
+let face5=new Image()
+	face5.src="http://www.blueb.co.kr/SRC/javascript/image1/d5.gif"
+	
+let face6=new Image()
+	face6.src="http://www.blueb.co.kr/SRC/javascript/image1/d6.gif"
+		
+//--------------------------------------(위)주사위 이미지 (아래)게임 기능--------------
 $(document).ready(function() {
 	
 	// 준비 완료 
@@ -7,15 +26,29 @@ $(document).ready(function() {
 		let beforePoint = 1;
 		let afterPoint = 1;
 
+	//------------------게임 시작 전------------------------
+		//게임 시작전 버튼 숨기기
+		$("#play").hide()
+		$("#1P").hide()
+		$("#2P").hide()
+		$("#3P").hide()
+		$("#4P").hide()
+		$("#mydice11").hide()
+		$("#mydice22").hide()
+	//------------------게임 시작 후------------------------
 		// 플레이 버튼 클릭 
-		$("#play").click(function() {
-			//1번째 주사위
-			let ran1 = Math.floor((Math.random() * 6) + 1); //1~6
-			console.log(ran1);
-			//2번째 주사위
-			let ran2 = Math.floor((Math.random() * 6) + 1); //1~6
-			console.log(ran2);
-			//주사위 합
+		$("#play").click(function() {		
+			
+			//첫번째 주사위 값
+			let ran1=Math.floor((Math.random()*5)+1)
+			//첫번째 주사위 이미지 보내기
+			document.images["mydice1"].src=eval("face"+ran1+".src")
+			
+			//두번째 주사위 값
+			let ran2=Math.floor((Math.random()*5)+1)
+			//두번째 주사위 이미지 보내기
+			document.images["mydice2"].src=eval("face"+ran2+".src")
+			
 			let Sran = ran1 + ran2
 			
 			//#dice <-html 출력화면에 주사위 수를 나타냄.
@@ -31,7 +64,7 @@ $(document).ready(function() {
 			console.log($("#point").attr("value"))
 			// 말을 이동시킵니다.
 			let afterId = "#p" + afterPoint;
-			$(afterId).append('<b class="horses" >◆1번마</b>');
+			$(afterId).append('<b class="horses">🏇</b>');
 
 			let beforeId = "#p" + beforePoint;
 			$(beforeId).empty();
